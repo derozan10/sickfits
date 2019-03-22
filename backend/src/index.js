914f83628134185e04473f7ccd49d0d86ebb92f9
derozan10 <lucas.vanremoortere@gmail.com>
2019-03-22 13:09:12 +0100

    added eslint and made server ready to start on backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,18 @@
 // let's go!
+require("dotenv");
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+server.start(
+    {
+        cors: {
+            credentials: true,
+            origin: process.env.FRONTEND_URL
+        }
+    },
+    deets => {
+        console.log(`server is running on port http://localhost:${deets.port}`);
+    }
+);
